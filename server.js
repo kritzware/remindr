259b3662a9fdf943da6522ac4dc493fcbe0b8509
kritzware <suspiciouspotato@gmail.com>
2016-08-27 02:23:30 +0100

    Added webhook route

--- server.js
+++ server.js
@@ -3,9 +3,20 @@ var app = express();
 var server = require('http').Server(app);
 
 const port = 3002
+const VALIDATION_TOKEN = 'remindr_bot_verify_token'
 
 app.get('/', (req, res) => {
 	res.json({Hello: 'world!'})
+})
+
+app.get('/webhook', (req, res) => {
+	if(req.query['hub.mode'] === 'subscribe' && req.query['hub.verify_token'] === VALIDATION_TOKEN) {
+		console.log('validating webhook')
+		res.status(200).send(req.query['hub.challenge'])
+	} else {
+	    console.error("Failed validation. Make sure the validation tokens match.");
+	    res.sendStatus(403);  
+	}
 })
 
 server.listen(port, function() {
